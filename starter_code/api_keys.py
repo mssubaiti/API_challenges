774e6028b1863760a9bfb85b88dbd28ff87f3d55
madisusan <m.s.subaiti@student.ie.edu>
2023-04-13 18:15:41 -0500

    scripts

---
+++ starter_code/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "337518282142b63cf5aefdada1c05413"
+
+# Geoapify API Key
+geoapify_key = "dc31d52da3404523895dcfc769f81702"
+
+
+#c19bd9e4dfea42778f043eb82787fecf
